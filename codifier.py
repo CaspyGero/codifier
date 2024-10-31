cc786f502b3094a5a27d2c48afe44b8dc20ed7db
Caspy <caspygerohunter@proton.me>
2024-10-30 21:41:06 -0300

    It works (Good enough) now

--- codifier.py
+++ codifier.py
@@ -1,5 +1,15 @@
-def maper(method: str, numberList: list):
-    methodCodeName = ["alfa", "bravo", "charlie", "delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
+def getPermutations(base: list) -> list:
+    if len(base) == 1:
+        return [base]
+    permutations = []
+    for i in range(len(base)):
+        actualIndex = [base[i]]
+        resto = base[:i] + base[i+1:]
+        for p in getPermutations(resto):
+            permutations.append(actualIndex + p)
+    return permutations
+def maper(method: str, numberList: list) -> dict:
+    methodCodeName = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
     """COMBINATIONS:
     A: [32 - 47]
     B: [48 - 57]
@@ -10,31 +20,29 @@ def maper(method: str, numberList: list):
     G: [123 - 126]
     """
     combinations = {
-        "a": {chr(i): i for i in range(32, 48)},
-        "b": {chr(i): i for i in range(48, 58)},
-        "c": {chr(i): i for i in range(58, 65)},
-        "d": {chr(i): i for i in range(65, 91)},
-        "e": {chr(i): i for i in range(91, 97)},
-        "f": {chr(i): i for i in range(97, 123)},
-        "g": {chr(i): i for i in range(123, 127)}
+        "a": [chr(i) for i in range(32, 48)],
+        "b": [chr(i) for i in range(48, 58)],
+        "c": [chr(i) for i in range(58, 65)],
+        "d": [chr(i) for i in range(65, 91)],
+        "e": [chr(i) for i in range(91, 97)],
+        "f": [chr(i) for i in range(97, 123)],
+        "g": [chr(i) for i in range(123, 127)]
     }
     methods = {}
-    #FIXME: DOESNT WORK AS EXPECTED
-    from itertools import product
-    lists = [combinations["a"], combinations["b"], combinations["c"], combinations["d"], combinations["e"], combinations["f"], combinations["g"]]
-    combinations = list(product(*lists))
+    permutations = getPermutations([combinations["a"], combinations["b"], combinations["c"], combinations["d"], combinations["e"], combinations["f"], combinations["g"]])
+    for i in range(len(permutations)):
+        permutations[i] = [item for sublist in permutations[i] for item in sublist]
     for i in range(0, len(methodCodeName) ** 2):
-        methods[methodCodeName[i // len(methodCodeName)] + methodCodeName[i % len(methodCodeName)]] = combinations[i]
-    #FIXME: IDK IF THIS IS THE BEST WAY
+        methods[methodCodeName[i // len(methodCodeName)] + methodCodeName[i % len(methodCodeName)]] = permutations[i]
     characterDictionary = {i: None for i in methods[method]}
     temporalIndex = 0
     for i in characterDictionary:
         characterDictionary[i] = numberList[temporalIndex]
         temporalIndex += 1
     return characterDictionary
-def codeGenerator():
+def codeGenerator() -> list:
     from random import randint
-    methodCodeName = ["alfa", "bravo", "charlie", "delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
+    methodCodeName = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
     normalList = [i for i in range(32, 127)]
     randomList = []
     for i in range(0, len(normalList)):
@@ -43,10 +51,10 @@ def codeGenerator():
     for i in range(0, 2):
         method += methodCodeName[randint(0, len(methodCodeName) - 1)]
     return method, randomList
-def codifier(method : str):
+def codifier(method: str, password: list, character: str) -> str:
     """
     method variable should contain the method used to create the dictionary, for example:
-    alfa: {chr(i): i for i in range(32, 127)}
+    alfaalfa: {chr(i): i for i in range(32, 127)}
     """
     password = codeGenerator()
 #TODO: DELETE AFTER
